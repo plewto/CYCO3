03f6e80c08be6255721f643017b272e49c0be473
sj <plewto@gmail.com>
2018-12-05 06:45:46 -0600

    Adds simple plugin 'registry' to prevent multiple reloads.

--- src/plugins.lisp
+++ src/plugins.lisp
@@ -5,28 +5,38 @@
 (setf *plugin-directory* (join-path *config-directory* "plugins" :as-file))
 (setf *current-plugin* nil)
 
-(defun load-plugin-file (filename)
-  (let ((fqn (join-path *plugin-directory*
-			*current-plugin*
-			filename :as-file)))
-    (if (or (probe-file fqn)
-	    (probe-file (append-filename-extension fqn ".lisp")))
+
+(let ((registry '()))
+
+  (defun load-plugin-file (filename)
+    (let ((fqn (join-path *plugin-directory*
+			  *current-plugin*
+			  filename :as-file)))
+      (if (or (probe-file fqn)
+	      (probe-file (append-filename-extension fqn ".lisp")))
+	  (progn
+	    (format t "Loading plugin file: ~A~%" fqn)
+	    (load fqn)
+	    t)
+	(cyco-warning
+	 (sformat "Plugin file ~S does not exists." fqn)))))
+
+  (defun forget-plugins ()
+    (setf registry '()))
+  
+  (defun plugin (name &optional reload)
+    (if (or reload (not (member name registry)))
 	(progn
-	  (format t "Loading plugin file: ~A~%" fqn)
-	  (load fqn)
-	  t)
-      (cyco-warning
-       (sformat "Plugin file ~S does not exists." fqn)))))
-
-(defun plugin (name)
-  (setf name (string-downcase name))
-  (let ((main-file (str+ name "-main")))
-    (setf *current-plugin* name)
-    (load-plugin-file main-file)))
-
-(defun sub-plugin (name)
-  (let ((temp *current-plugin*))
-    (plugin name)
-    (setf *current-plugin* temp)))
+	  (push name registry)
+	  (setf name (string-downcase name))
+	  (let ((main-file (str+ name "-main")))
+	    (setf *current-plugin* name)
+	    (load-plugin-file main-file)))
+      name))
+  
+  (defun sub-plugin (name)
+    (let ((temp *current-plugin*))
+      (plugin name)
+      (setf *current-plugin* temp))) )
     
   

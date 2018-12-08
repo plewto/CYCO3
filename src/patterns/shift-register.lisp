ee0c198fba80e58fbc4da449bb4cfbae501298b9
sj <plewto@gmail.com>
2018-12-08 00:21:51 -0600

    Reimplements shift-register with 16-bit register.:

--- src/patterns/shift-register.lisp
+++ src/patterns/shift-register.lisp
@@ -1,121 +1,142 @@
 ;;;; CYCO shift-register
 ;;;;
-;;;; 8-bit linear-feedback shift-register
+;;;; 
 
-
-(constant +shift-register-length+ 8)
+(constant +shift-register-length+ 16)
 
 (defclass shift-register (pattern)
-  ((register
+  ((length
+    :allocation :class
     :type integer
-    :accessor shift-register-register
-    :initform 1
+    :reader sr-length
+    :initform +shift-register-length+)
+   (register
+    :type integer
+    :accessor sr-register
+    :initform #b1
     :initarg :seed)
    (seed
     :type integer
-    :accessor shift-register-seed
-    :initform 1
+    :reader sr-seed
+    :initform #b1
     :initarg :seed)
-   (prerun
-    :type integer
-    :reader shift-register-prerun
-    :initform 10
-    :initarg :prerun)
    (taps
     :type integer
-    :accessor shift-register-taps
-    :initform 0
+    :accessor sr-taps
+    :initform #b1000000000000000
     :initarg :taps)
    (mask
     :type integer
-    :accessor shift-register-mask
-    :initform #b11111111
+    :accessor sr-mask
+    :initform #b1111111111111111
     :initarg :mask)
    (base
     :type integer
-    :accessor shift-register-base
-    :initform 10
+    :accessor sr-base
+    :initform #xffff
     :initarg :base)
    (bias
-    :type integer
-    :accessor shift-register-bias
+    :type initarg
+    :accessor sr-bias
     :initform 0
-    :initarg :bias)))
+    :initarg :bias)
+   (prerun
+    :type integer
+    :accessor sr-prerun
+    :initform 0
+    :initarg :prerun)))
 
 (defmethod shift-register-p ((sr shift-register)) t)
 
 (defmethod reset ((sr shift-register))
-  (setf (shift-register-register sr)
-	(shift-register-seed sr))
-  (dotimes (i (shift-register-prerun sr))(next-1 sr))
+  (setf (sr-register sr)(sr-seed sr))
+  (dotimes (i (sr-prerun sr))(next-1 sr))
   sr)
 
-(defun shift-register (seed taps &key (mask #b11111111)(base 12)(bias 0)(prerun 0))
-  "Creates new SHIFT-REGISTER pattern
-seed - initial value,  0 < seed <= #xff
-taps - feedback taps,  0 <= taps <= #xff
-mask - mask applied to output value. 0 < mask <= #xff
-base - output value is modulo base.  0 < base
-bias - amount added to output value
-prerun - number of times to shift register after a reset or construction.
-
-current value <-- ([register] and mask) mod base) + bias
-"
+(defun shift-register (seed taps &key
+			    (mask #b1111111111111111)
+			    (base #xffff)
+			    (bias 0)
+			    (prerun 0))
   (reset (make-instance 'shift-register
-			:seed (logand seed #xff)
-			:prerun prerun
-			:taps (logand taps #xff)
+			:seed (logand seed #xffff)
+			:taps (logand taps #xffff)
 			:mask mask
 			:base base
 			:bias bias
 			:prerun prerun)))
-  
+
 (defmethod clone ((sr shift-register) &key new-name new-parent)
   (dismiss new-name new-parent)
-  (shift-register (shift-register-seed sr)
-		  (shift-register-taps sr)
-		  :mask (shift-register-mask sr)
-		  :base (shift-register-base sr)
-		  :bias (shift-register-bias sr)
-		  :prerun (shift-register-prerun sr)))
+  (shift-register (sr-seed sr)
+		  (sr-taps sr)
+		  :mask (sr-mask sr)
+		  :base (sr-base sr)
+		  :bias (sr-bias sr)
+		  :prerun (sr-prerun sr)))
 
 (defmethod value ((sr shift-register))
-  (let ((mask (shift-register-mask sr))
-	(base (shift-register-base sr))
-	(bias (shift-register-bias sr)))
-    (+ (rem (logand mask (shift-register-register sr)) base) bias)))
+  (let ((mask (sr-mask sr))
+	(base (sr-base sr))
+	(bias (sr-bias sr)))
+    (+ (rem (logand mask (sr-register sr)) base) bias)))
+    
+;; (defmethod shift-register-feedback ((sr shift-register) &optional (insert 0))
+;;   (let ((acc insert)
+;; 	(reg (sr-register sr))
+;; 	(taps (sr-taps sr)))
+;;     (dotimes (bit (sr-length sr))
+;;       (let ((fb (logand (logand reg 1)
+;; 			(logand taps 1))))
+;; 	;; (format t "DEBUG bit ~2D FB ~D  reg ~16B taps ~16B~%" bit fb reg taps)
+;; 	(setf acc (if (zerop fb) 0 1)
+;; 	      reg (ash reg -1)
+;; 	      taps (ash taps -1))))
+;;     (logand acc 1)))
 
-
-(defmethod shift-register-feedback ((sr shift-register))
-  (let ((acc 0)
-	(reg (shift-register-register sr))
-	(tap (shift-register-taps sr)))
-    (dotimes (bit +shift-register-length+)
-      (let ((fb (logand (logand reg 1)
-			(logand tap 1))))
-	(if (not (zerop fb))(setf acc (1+ acc)))
-	(setf reg (ash reg -1)
-	      tap (ash tap -1))))
+(defmethod shift-register-feedback ((sr shift-register) &optional (insert 0))
+  (let* ((a (sr-register sr))
+	 (b (sr-taps sr))
+	 (c (logand #xffff (+ (logand a b)(logand insert 1))))
+	 (acc 0))
+    (while (plusp c)
+      (setf acc (+ acc (if (zerop (logand c 1)) 0 1)))
+      (setf c (ash c -1)))
     (logand acc 1)))
-	    
-(defmethod next-1 ((sr shift-register))
-  (let ((fb (shift-register-feedback sr)))
-    (setf (shift-register-register sr)
-	  (logior (ash (shift-register-register sr) 1) fb)))
+
+
+(defmethod shift-1 ((sr shift-register) &optional (insert 0))
+  (let ((fb (shift-register-feedback sr insert)))
+    (setf (sr-register sr)
+	  (logior (ash (sr-register sr) 1) fb)))
   (value sr))
 
-(flet ((format-binary
-	(byte)
-	(let ((acc ""))
-	  (dotimes (bit +shift-register-length+)
-	    (let ((probe (expt 2 bit)))
-	      (setf acc (str+ (if (zerop (logand probe byte)) "0" "1") acc))))
-	  acc)))
+(defmethod next-1 ((sr shift-register))
+  (shift-1 sr 0))
 
-  (defmethod dump-shift-register ((sr shift-register))
-    (format t "SHIFT-REGISTER~%")
-    (format t "Register : ~A~%" (format-binary (shift-register-register sr)))
-    (format t "Taps     : ~A~%" (format-binary (shift-register-taps sr)))
-    (format t "Feedback : ~A~%" (shift-register-feedback sr))
-    (format t "Seed     : ~A~%" (format-binary (shift-register-seed sr)))
-    (format t "Mask     : ~A~%" (format-binary (shift-register-mask sr)))))
+(defmethod find-shift-register-period ((sr shift-register) &key 
+				       (prerun 100)(guard 8000))
+  (let ((seen (make-hash-table))
+	(count 0))
+    (dotimes (i prerun)(shift-1 sr))
+    (while (< count guard)
+      (let ((current (value sr)))
+	(if (gethash current seen)
+	    (return-from find-shift-register-period count))
+	  (progn
+	    (setf (gethash current seen) t)
+	    (setf count (1+ count))
+	    (shift-1 sr))))
+    nil))
+
+(defmethod dump-shift-register ((sr shift-register))
+  (format t "SHIFT-REGISTER~%")
+  (format t "    Register : ~A~%" (format-binary (sr-register sr)))
+  (format t "    Taps     : ~A~%" (format-binary (sr-taps sr)))
+  (format t "    Feedback : ~A~%" (format-binary (shift-register-feedback sr)))
+  (format t "    Seed     : ~A~%" (format-binary (sr-seed sr)))
+  (format t "    Mask     : ~A~%" (format-binary (sr-mask sr)))
+  (format t "    Base     :  ~A~%" (sr-base sr))
+  (format t "    Bias     :  ~A~%" (sr-bias sr))
+  (format t "    Prerun   :  ~A~%" (sr-prerun sr)))
+

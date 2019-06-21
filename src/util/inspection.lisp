02ee387cd3ada6a9d56d0dcf3f965e92311a2d7b
Steven Jones <plewto@gmail.com>
2019-06-21 13:30:27 -0500

    Defines various new 'help/inspection' functions.

--- src/util/inspection.lisp
+++ src/util/inspection.lisp
@@ -12,19 +12,19 @@
 (defmethod ? ((n integer))
   (format t "INTEGER~%"))
 
-(defun ?tree (&optional (what 'o))
-  "Displays tree structure.
-Optional what argument indicates which tree to display
-'o - show orchestra tree starting with +root-instrument+
-'p - show project tree using *project*"
-  (let ((options '(o p)))
-    (cond ((eq what 'p)   			; project
-	   (if (project-p *project*)
-	       (print-tree *project*)
-	     (format t "There is no current project.~%")))
-	  ((eq what 'o)			        ; orchestra
-	   (print-tree +root-instrument+))
-	  (t "Expected optional argument on of ~A~%" options))))
+;; (defun ?tree (&optional (what 'o))
+;;   "Displays tree structure.
+;; Optional what argument indicates which tree to display
+;; 'o - show orchestra tree starting with +root-instrument+
+;; 'p - show project tree using *project*"
+;;   (let ((options '(o p)))
+;;     (cond ((eq what 'p)   			; project
+;; 	   (if (project-p *project*)
+;; 	       (print-tree *project*)
+;; 	     (format t "There is no current project.~%")))
+;; 	  ((eq what 'o)			        ; orchestra
+;; 	   (print-tree +root-instrument+))
+;; 	  (t "Expected optional argument on of ~A~%" options))))
 
 (let ((ary (->vector (copies 16 '()))))
   (labels ((walk (inst)
@@ -44,4 +44,22 @@ Optional what argument indicates which tree to display
 	  (dolist (iname ilst)
 	    (format t "[~2D] ~A~%" chan iname)))))))
 
+
+(defun ?o (&optional (root +root-instrument+))
+  "Displays orcestra tree."
+  (print-tree root))
+
+(defun ?p ()
+  "Displays project structure."
+  (if *project*
+      (print-tree *project*)
+    (format t "There is no current project, *PROJECT* is NIL~%")))
+
+(defun ?? ()
+  "Displays list of available inspection functions."
+  (format t "(? object)   Displays info about object.~%")
+  (format t "(?a symbol)  Shortcut for (apropos symbal)~%")
+  (format t "(?d symbol)  Shortcut for (describe symbol)~%")
+  (format t "(?o)         Displays orchestra tree.~%")
+  (format t "(?p)         Displays current project structure.~%"))
 

87135f12daa6695971c2d7c863efd472f56e22cf
Steven Jones <plewto@gmail.com>
2019-07-02 14:36:32 -0500

    Simplifies create-project-framework, defines ?projects.

--- src/composition/project.lisp
+++ src/composition/project.lisp
@@ -1,4 +1,4 @@
-;;;; CYCO
+;;; CYCO
 ;;;; 
 
 (constant +project-properties+
@@ -14,7 +14,7 @@
 
 (defstruct seq-mode
   section-name
-  shift ;; initiasl time offset what units?
+  shift ;; initial time offset what units?
   count ;; <= 0 --> skip
   transpose
   invert
@@ -460,25 +460,33 @@ file is  <user-home>/cyco-projects/foo/MIDI/foo.mid"
     smf))
 
 
+
 (flet ((format-name (sym)
 		    (string-downcase (->string sym))))
 
-  (defun create-project-framework (project-name &rest extras)
-    "Creates project directories and specified (empty) files.
-project-name - quoted symbol
+  (defun create-project-framework (files)
+    "Creates empty project directory.
+The files argument specifies the project name and any additional empty files 
+to be created.   
 
-Each additional argument must be a quoted symbol and is used to create
-a matching project-file with the same name.   The main project file
-is automatically created.   Existing files are not overwritten."
-    (let* ((pname (format-name project-name))
+(create-project-framework 'foo)
+    Create project directory foo with main file and MIDI directory.
+
+(create-project-framework '(foo bar baz))
+    As above but also creates empty files bar.lisp and baz.lisp"
+
+    (let* ((flist (->list files))
+	   (project-name (car flist))
+	   (extras (cdr flist))
+	   (pname (format-name project-name))
+	   (main-file (sformat *project-main-filename-format* project-name))
 	   (pdir (join-path *default-project-directory* pname))
 	   (outdir (join-path pdir *default-project-output-directory*)))
       (format t "Creating project ~A directory framework~%" project-name)
       (dolist (d (list pdir outdir))
 	(format t "Ensuring directory exist: ~A~%" d)
 	(ensure-directories-exist d))
-      (dolist (fn (cons (sformat *project-main-filename-format* project-name)
-			extras))
+      (dolist (fn (cons main-file (->list extras)))
 	(let ((fqn (append-filename-extension
 		    (join-path pdir (format-name fn) :as-file)
 		    ".lisp")))
@@ -490,11 +498,17 @@ is automatically created.   Existing files are not overwritten."
 				    :if-does-not-exist :create)))
 		  (format stream ";;;; CYCO Project ~A  File ~A.lisp~%" pname (format-name fn))
 		  (format stream ";;;;~%~%")
-		  (format stream (sformat "(version ~A)~%~%" (car +cyco-version+)))
-		  (format stream (sformat "(project ~A)~%" project-name))
+		  (if (string= fn main-file)
+		      (progn
+		  	(format stream (sformat "(version ~A)~%~%" (car +cyco-version+)))
+		  	(format stream (sformat "(project ~A)~%" project-name))))
 		  (close stream)))
   	    (format t "Project file already exists: ~A~%" fqn)))))))
 
-
-
-
+(defun ?projects ()
+  (format t "Contents of project directory ~A~%" *default-project-directory*)
+  (dolist (fqn (directory (sformat "~a/*" *default-project-directory*)))
+    (let* ((nstr (namestring fqn))
+	   (pname (second (split-path (subseq nstr 0 (1- (length nstr)))))))
+      (format t "  ~A~%" pname))))
+	   

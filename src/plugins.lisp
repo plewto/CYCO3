566a621e26b4df8cc594182f21034db9cf80f025
Steven Jones <plewto@gmail.com>
2019-07-02 13:51:05 -0500

    Defines ?plugins function.

--- src/plugins.lisp
+++ src/plugins.lisp
@@ -9,6 +9,7 @@
 (let ((registry '()))
 
   (defun load-plugin-file (filename)
+    "Load lisp file relative to current plugin directory."
     (let ((fqn (join-path *plugin-directory*
 			  *current-plugin*
 			  filename :as-file)))
@@ -22,9 +23,14 @@
 	 (sformat "Plugin file ~S does not exists." fqn)))))
 
   (defun forget-plugins ()
+    "Clear plugin registry.
+Note forget-plugin does not un-define active plugin definitions, all
+it does is clear the plugin registry."
     (setf registry '()))
   
   (defun plugin (name &optional reload)
+    "Load named plugin.
+Loaded plugins are not reloaded unless optional reload is true."
     (if (or reload (not (member name registry)))
 	(progn
 	  (push name registry)
@@ -35,8 +41,19 @@
       name))
   
   (defun sub-plugin (name)
+    "Load a plugin as a 'sub-plugin' of the current plugin."
     (let ((temp *current-plugin*))
       (plugin name)
-      (setf *current-plugin* temp))) )
+      (setf *current-plugin* temp)))
+
+  (defun ?plugins ()
+    "Display list of loaded and available plugins."
+    (format t "Plugins  '+' indicates plugin has been loaded.~%")
+    (dolist (fqn (directory (sformat "~a/*" *plugin-directory*)))
+      (let* ((nstr (namestring fqn))
+	     (plugin-name (->symbol (second (split-path (subseq nstr 0 (1- (length nstr))))))))
+	(format t "~A " (if (member plugin-name registry) "+" " "))
+	(format t "~A~%" plugin-name)))) )
+
     
   

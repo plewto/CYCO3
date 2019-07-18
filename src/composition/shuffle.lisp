d0d6a3ec50b919a085d4df2beaafbb410faca8e2
Steven Jones <plewto@gmail.com>
2019-07-18 01:52:20 -0500

    Syntax typo fix.

--- src/composition/shuffle.lisp
+++ src/composition/shuffle.lisp
@@ -12,7 +12,7 @@ the cue function currently in use.
 The default no-shuffle function always returns 0.0
 
 Shuffle applies to either QBALL or Strummer part events and may 
-be set at any level in project tree."
+be set at any level in project tree."))
        
   (defun no-shuffle (time-specification)
     (dismiss time-specification)

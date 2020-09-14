8e8ed667b4ed667d9bb99ba1c42b790437639360
Steven Jones <plewto@gmail.com>
2020-09-14 11:08:06 -0500

    Minor bugfix osc plugin.

--- plugins/osc/osc-main.lisp
+++ plugins/osc/osc-main.lisp
@@ -14,13 +14,12 @@
 	:cyco)
 
 (global *local-host* #(127 0 0 1))
-
 (global *default-osc-send-host* *local-host*)
-(global *default-osc-send-port* 65000)
-(global *default-osc-send-address* "pigiron")
-(global *osc-receive-port* *default-osc-send-port*)
-(global *osc-receive-address* "CYCO")
-(global *osc-receive-buffer-length* 1024)
+(global *default-osc-send-port* 6000)
+(global *default-osc-send-address* "pig/op/MidiPlayer")
+;; (global *osc-receive-port* *default-osc-send-port*)
+;; (global *osc-receive-address* "CYCO")
+;; (global *osc-receive-buffer-length* 1024)
 (global *osc-send-verbose* t)
 
 (load-plugin-file "send")

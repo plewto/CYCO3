f63aa1b1d20ea0cc781c0a0be7af7e9457d0b891
Steven Jones <plewto@gmail.com>
2021-03-31 13:17:03 -0500

    Updates test for new meta-channel signature.

--- test/12-channels.lisp
+++ test/12-channels.lisp
@@ -9,16 +9,14 @@
 (set-meta-channel 'apple 1)
 (set-meta-channel 'rim-shot 'snare)
 
-
-
 (pass? "meta-channel test 12.1"
        (and (= (meta-channel 3) 3)
 	    (= (meta-channel 'alpha) 1)
 	    (= (meta-channel 'beta) 2)
 	    (= (meta-channel 'drums) 10)
-	    (eq (meta-channel 'snare nil) 'drums)
-	    (eq (meta-channel 'rim-shot nil) 'snare)
-	    (= (meta-channel 'rim-shot t) 10)))
+	    (eq (meta-channel 'snare :resolve nil) 'drums)
+	    (eq (meta-channel 'rim-shot :resolve nil) 'snare)
+	    (= (meta-channel 'rim-shot :resolve t) 10)))
 
 (pass? "meta-channel test 12.2"
        (and (every #'meta-channel-assignment-p (range 1 17))

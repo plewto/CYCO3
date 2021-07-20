0d424d84c36a10953c70d523764d522d471b0338
Steven Jones <plewto@gmail.com>
2021-07-20 11:50:50 -0500

    Sets bootstrap load mode to quite.

--- src/bootstrap.lisp
+++ src/bootstrap.lisp
@@ -1,5 +1,30 @@
 (in-package :cyco)
 
+;; Taken from Rob Warnock's post "How to programmatically exit?"
+;; https://groups.google.com/forum/#!msg/comp.lang.lisp/kpTqyLQ-HaU/ms2NYHmlyZQJ
+;;
+(defun exit (&optional code)
+      ;; This group from "clocc-port/ext.lisp"
+      #+allegro (excl:exit code)
+      #+clisp (#+lisp=cl ext:quit #-lisp=cl lisp:quit code)
+      #+cmu (ext:quit code)
+      #+cormanlisp (win32:exitprocess code)
+      #+gcl (lisp:bye code)                     ; XXX Or is it LISP::QUIT?
+      #+lispworks (lw:quit :status code)
+      #+lucid (lcl:quit code)
+      #+sbcl (sb-ext:exit :code code)
+      ;; This group from Maxima
+      #+kcl (lisp::bye)                         ; XXX Does this take an arg?
+      #+scl (ext:quit code)                     ; XXX Pretty sure this *does*.
+      #+(or openmcl mcl) (ccl::quit)
+      #+abcl (cl-user::quit)
+      #+ecl (si:quit)
+      ;; This group from <hebi...@math.uni.wroc.pl>
+      #+poplog (poplog::bye)                    ; XXX Does this take an arg?
+      #-(or allegro clisp cmu cormanlisp gcl lispworks lucid sbcl
+            kcl scl openmcl mcl abcl ecl)
+      (error 'not-implemented :proc (list 'exit code))) 
+
 (let* ((current "")
        (verbose-flag t)
        (print-flag nil)
@@ -150,27 +175,4 @@
 
 (build-cyco)
 
-;; Taken from Rob Warnock's post "How to programmatically exit?"
-;; https://groups.google.com/forum/#!msg/comp.lang.lisp/kpTqyLQ-HaU/ms2NYHmlyZQJ
-;;
-(defun exit (&optional code)
-      ;; This group from "clocc-port/ext.lisp"
-      #+allegro (excl:exit code)
-      #+clisp (#+lisp=cl ext:quit #-lisp=cl lisp:quit code)
-      #+cmu (ext:quit code)
-      #+cormanlisp (win32:exitprocess code)
-      #+gcl (lisp:bye code)                     ; XXX Or is it LISP::QUIT?
-      #+lispworks (lw:quit :status code)
-      #+lucid (lcl:quit code)
-      #+sbcl (sb-ext:exit :code code)
-      ;; This group from Maxima
-      #+kcl (lisp::bye)                         ; XXX Does this take an arg?
-      #+scl (ext:quit code)                     ; XXX Pretty sure this *does*.
-      #+(or openmcl mcl) (ccl::quit)
-      #+abcl (cl-user::quit)
-      #+ecl (si:quit)
-      ;; This group from <hebi...@math.uni.wroc.pl>
-      #+poplog (poplog::bye)                    ; XXX Does this take an arg?
-      #-(or allegro clisp cmu cormanlisp gcl lispworks lucid sbcl
-            kcl scl openmcl mcl abcl ecl)
-      (error 'not-implemented :proc (list 'exit code))) 
+

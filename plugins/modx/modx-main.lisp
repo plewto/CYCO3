c25d5f9ac19e2dbc75845d9548c8d44920bc061a
Steven Jones <plewto@gmail.com>
2021-08-29 14:08:12 -0500

    Creates plugin for Yamaha MODX

--- plugins/modx/modx-main.lisp
+++ plugins/modx/modx-main.lisp
@@ -15,11 +15,13 @@
 ;;;;
 ;;;; 1) Create a modx-performance instrument.
 ;;;;    Use this for program-change events and as parent to
-;;;;    specific instruments.
+;;;;    specific instruments.  The modx-performance macro
+;;;;    binds the new performance to *current-modx-performance*
 ;;;;
-;;;; 2) Create 1 or more instruments under the modx-performance,
-;;;;    each on specific MIDI channel.
-;;;;
+;;;; 2) Use modx-instrument to create 1 or more instruments under
+;;;;    the modx-performance, each on specific MIDI channel.
+;;;;    The performance keyword argument to modx-instrument
+;;;;    defaults to *current-modx-performance*
 
 (in-package :cyco)
 
@@ -37,6 +39,8 @@
 	    :channel 1
 	    :transient nil)
 
+(param *current-modx-performance* nil)
+
 
 (defmacro modx-performance (name bank-msb bank-lsb program-number)
   `(let ((inst (make-instrument ',name
@@ -44,16 +48,18 @@
 				:transient t)))
      (set-program-map inst (modx-program-map ,bank-msb ,bank-lsb ,program-number))
      (defparameter ,name inst)
+     (setf *current-modx-performance* inst)
      inst))
 
 
-(defmacro modx-instrument (name modx-performance channel &key
+(defmacro modx-instrument (name channel &key
+				(performance nil)
 				(keynumber-map (basic-keynumber-map))
 				(dynamic-map (basic-dynamic-map))
 				(articulation-map (basic-articulation-map))
 				(remarks ""))
   `(let ((inst (make-instrument ',name
-				:parent ,modx-performance
+				:parent (or ,performance *current-modx-performance*)
 				:channel ,channel
 				:keynumber-map ,keynumber-map
 				:dynamic-map ,dynamic-map
@@ -62,3 +68,6 @@
 				:remarks (->string (or ,remarks "")) )))
      (defparameter ,name inst)
      inst))
+
+(load-plugin-file "drumkits/real-brushes-kit")
+(load-plugin-file "drumkits/iranian-mix-kit")

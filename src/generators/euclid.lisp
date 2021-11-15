19da4090ed46b2cd48b37ab0be248b92f3afd568
Steven Jones <plewto@gmail.com>
2021-11-14 20:31:34 -0600

    Adds new CUE-FUNCTION method to EUCLID class.

--- src/generators/euclid.lisp
+++ src/generators/euclid.lisp
@@ -63,12 +63,6 @@ https://en.wikipedia.org/wiki/Euclidean_rhythm"))
 	  :monitor (monitor mother)
 	  :action (action mother)))
 
-;; (defmethod value ((gen euclid))
-;;   (funcall (slot-value gen 'value-hook)
-;; 	   (mod (+ (slot-value gen 'shift)
-;; 		   (round (slot-value gen 'internal-value)))
-;; 		(slot-value gen 'length))))
-
 (defmethod value ((gen euclid))
   (funcall (slot-value gen 'value-hook)
 	   (+ (slot-value gen 'shift)
@@ -87,11 +81,11 @@ https://en.wikipedia.org/wiki/Euclidean_rhythm"))
 
 ;; NOTES: Returns incorrect value when points > length.
 ;;
-(defmethod pattern-length ((eclid euclid) &key &allow-other-keys)
+(defmethod pattern-length ((gen euclid) &key &allow-other-keys)
   (let* ((guard 5000)
 	 (seen '())
 	 (points 0)
-	 (gen (clone eclid))
+	 (gen (clone gen))
 	 (value 0))
     (while (not (and (member value seen)(< points guard)))
       (push value seen)
@@ -100,14 +94,54 @@ https://en.wikipedia.org/wiki/Euclidean_rhythm"))
     (1- points)))
 
 
+(defmethod cue-function ((gen euclid))
+  "Returns cue-function  for this specific instance of Euclid."
+  #'(lambda (time-signature time-cue)
+      (cue-n time-signature (list 0 time-cue (slot-value gen 'length)))))
+
+
 (setf (documentation 'euclid 'function)
       "Creates 'Euclidean Rhythm' Generator.
 
 https://en.wikipedia.org/wiki/Euclidean_rhythm
 https://dbkaplun.github.io/euclidean-rhythm/
 
-TODO Redo Euclid documentation (remove value mod expression)
+Euclid generates values by dividing length as evenly as possible into the
+given number of points. 
+
+LENGTH   - Integer length.  2 <= length
+POINTS   - Number of points length is divided by.
+           Most Euclidean algorithms restricts points to be less then or
+           equal to length.  This implementation allows values greater
+           then the length.  In theses cases duplicate values are
+           generated. 
+
+           (next (euclid 4 5) 5) --> (0 1 2 2 3)
+
+:SHIFT   - Offset added to all values.
+           (next (euclid 4 3) 3) --> (0 1 3)
+           (next (euclid 4 3 :shift 10)) --> (10 11 13)
+
+:HOOK    - Function applied to each value
+           (next (euclid 4 3 :hook #'(lambda (n)(* 100 n))) 3) --> (0 100 300)
+          
+The Euclidean results are particularly useful for generating rhythmic
+cue-list.   For example, assuming 4/4 time, a length of 16 may be treated 
+as the 16th notes of a bar (staring with 0).  The generated results make a
+selection of which 16th notes are played.
 
 
-" )
+Calling the CUE-FUNCTION method on an instance of Euclid returns a
+cuing-function specifically for that instance.
+
+
+(let* ((e (euclid 8 3)))
+    (qball foo snare
+       :bars 1
+       :cuefn (cue-function e)
+       :cue (next e 3)
+       :key 'hit))")
+
+
+
 

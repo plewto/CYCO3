27f95783bd957b3003b33bbac9176299cb1bf952
Steven Jones <plewto@gmail.com>
2022-01-09 16:25:39 -0600

    Simplifies Pig plugin.

--- plugins/pig/docs.lisp
+++ plugins/pig/docs.lisp
@@ -1,75 +1,121 @@
-;;;; CYCO pig plugin docs
+;;;; pig plugin docs
 ;;;;
 
-(in-package :pig)
+(constant +pig-docs+ "
+The PIG plugin provides an interface to the Pigiron MIDI utility,
+available at github.com/plewto/Pigiron.
+
+Global variables:
+
+    *pig-sleep-time*         - Time delay after stopping MIDI file player.
+    *pig-prefix*             - OSC prefix for the Pigiron server, default /pig
+    *pig-host*               - Pigiron host address, defaults to local machine, #(127 0 0 1)
+    *pig-port*               - Pigiron port number, default 8020.
+    *pig-output-channels*    - List of output MIDI channels.
+    *pig-midi-filename*      - Current MIDI file name.
+    *pig-player-op*          - Pigiron name for MIDIPlayer operator, default 'player'
+    *pig-output-op*          - Pigiron name for main MIDIOutput operator, default 'out'
+    *pig-distributor-op*     - Pigiron name form Distributor operator, default 'dist'
 
 
-(param *player-docs* "
-(STOP)  
-    Halt playback
-
-(PLAY &optional name)
-    Start playback. 
-    If optional name is suplied load corresponding MIDI file.
-    If name is reative, load from current projects MIDI directory.
-
-(PIG:LOAD-SMF name)
-    Load MIDI file into player.
-    name should be an absolute filename for a MIDI file.")
-
-(param *midi-docs* "
-The following functions transmit a single MIDI message.
-    1 <= channel <= 16
-    0 <= data < 128 for all data values
-
-    (PIG:NOTE-OFF (channel key &optional (velocity 0)))
-    (PIG:NOTE-ON (channel key &optional (velocity 64)))
-    (PIG:POLY-PRESSURE channel key pressure)
-    (PIG:CONTROLLER channel controller-number value)
-    (PIG:PROGRAM channel program-number)
-    (PIG:MONO-PRESSURE channel pressure)
-
-(PIG:BEND channel normal-bend)
-    Transmit single pitch bend message, -1.0 <= normal-bend <= +1.0
-
-(PIG:SYSEX bytes)
-    Transmnit single SYSEX message. 
-    bytes should not include SYSEX or EOX status bytes.")
+The Pigiron server is assumed to have a process graph similar to the following with 
+the appropriate Operator names.
 
 
-(param *monitor-docs* "
-(PIG:MONITOR-EXCLUDE status flag)
-    If flag is non-nil exclude status from the monitor output.
-
-(PIG:MONITOR-ON)
-    Enable MIDI monitor.
-
-(PIG:MONITOR-OFF)
-    Disable MIDI monitor.
-
-(PIG:MONITOR-LOG filename)
-    Open filename as monitor log.
-
-(PIG:MONITOR-CLOSE-LOG)
-     Close monitor logfile.")
+    MIDIInput -> ChannelFilter -> Distributor -> Monitor --+--> MIDIOutput
+                                                           |
+                                              MIDIPlayer --+
 
 
-(param *channel-docs* "
-(PIG:IN-CHANNELS (&rest CHANNELS))
-    Selects MIDI input channels.
-
-(PIG:OUT-CHANNELS (&rest CHANNELS)
-    Selects MIDI output channels.")
+The MIDIInput, ChannelFilter and Monitor operators are not directly referenced by the
+PIG plugin.
 
 
+The available functions are:
+
+    (PIG-SEND command &optional (data 0))
+        
+        Send command to Pigiron server, the data value must not be nil.
+
+    (PIG-PING)
+
+        Connectivity test between CYCO and Pigiron.   Upon execution Pigiron should 
+        print something like '/pig/ping' to it's terminal.
 
 
-(defun ?pig (&optional topic)
-  (let ((s (cond ((eq topic :player) *player-docs*)
-		 ((eq topic :midi) *midi-docs*)
-		 ((eq topic :monitor) *monitor-docs*)
-		 ((eq topic :channel) *channel-docs*)
-		 ((or (not topic)(eq topic :topics))
-		  "Select ?PIG topic  :PLAYER  :MIDI  :MONITOR  :CHANNEL"))))
-    (format t "~A~%" s)))
-		  
+    (PIG-LOAD-SMF name)
+
+        Load MIDI file into MIDI file player.
+        Name may be absolute filename or relative to the current project.
+        For relative names, do not include filename extension.
+
+    (STOP)
+
+        Stop Pigiron MIDI file playback.   CYCO execution stops for *pig-sleep-time* 
+        seconds.
+
+    (s)
+        A convenience alias for (STOP)
+
+
+    (PLAY &optional name)
+
+        Start playback of MIDI file.   If name is supplied load the indicated file first
+        using (PIG-LOAD-SMF).   Current playback is stopped and CYCO halts for a moment 
+        prior to commencing new playback.
+
+   (P &optional name)
+
+       A convenience alias for (PLAY name)
+
+     
+   (OUTPUT-CHANNEL c)
+
+      Sets output MIDI channel(s). 
+      This command is useful for setting the destination channels for any MIDI inputs.
+      The c argument may be an explicit MIDI channel or a CYCO instrument.   Multiple 
+      channels may be selected by using a list.
+
+      The MIDIPlayer is not effected.
+
+   
+   (PIG-NOTES keylist &key (vel 64)(dur 1.0))
+
+        Audition indicated notes on the current MIDI output channels.
+
+
+   (PIG-CC  controller value)
+
+        Transmits MIDI control-change events on current output channels.
+
+
+   (PIG-program number)
+
+        Transmits MIDI program-change on current output channels.  
+
+
+   (PIG-BEND value)
+      
+       Transmits Pitch bend on current output channels.  The value is normalized float
+       between -1.0 and +1.0.
+
+
+   (PIG-SYSEX bytes &optional raw)
+       Transmits system-exclusive message.
+       Normally, a SYSEX and END-OF-SYSEX status bytes are sent before and after the 
+       byte list.   Set the raw argument t to suppress these bytes.
+")
+
+
+(defun ?pig ()
+  (format t +pig-docs+))
+	  
+       
+      
+     
+       
+
+
+
+	  
+
--- plugins/pig/pig-main.lisp
+++ plugins/pig/pig-main.lisp
@@ -1,117 +1,9 @@
-;;;; CYCO pig plugin
+;;;; pig plugin
 ;;;;
-;;;; CYCO interface to pigiron
+;;;; CYCO interface to Pigiron
 ;;;; Pigiron is a MIDI routing utility with integrated MIDI file player,
 ;;;; available at github.com/plewto/Pigiron
 ;;;;
-;;;; Use function pig:server-proxy to set Pigiron server parameters.
-;;;; By default the server values are:
-;;;;
-;;;;    osc-prefix   /pig
-;;;;    host         #(127 0 0 1)
-;;;;    port         8020
-;;;;
-;;;;
-;;;;
-;;;; Assumed Pigiron MIDI process graph
-;;;;
-;;;;     --> ChannelFilter --> Distributor --+--> MIDIOutput --> Monitor
-;;;;                            MIDIPlayer --+         
-;;;;
-;;;;
-;;;;  Lisp Var        Opertator      Name
-;;;;  *midi-player*   MIDIPlayer     player
-;;;;  *midi-op*       MIDIOutput     out
-;;;;  *monitor*       Monitor        mon
-;;;;  *filter*        ChannelFilter  filter
-;;;;  *distributor*   Distributor    dist
-;;;;
-;;;; Usage from CYCO package
-;;;;
-;;;;   (play &optional name)
-;;;;       Optionally load MIDI file and begin playback.
-;;;;       If name is relative load it from the current-project's
-;;;;       MIDI directory.
-;;;;
-;;;;   (stop)
-;;;;       Stop MIDI player
-;;;;
-;;;;   (pig:load-smf name)
-;;;;       Load MIDI file into player.
-;;;;       If name is relative filename, load from current project's MIDI
-;;;;       directory.
-;;;;
-;;;;   (pig:ping)
-;;;;       Send 'ping' message to Pigiron.
-;;;;
-;;;;   (pig:exit)
-;;;;       Send 'exit' message to Pigiron.
-;;;;
-;;;;   (pig:info name)
-;;;;       Display info for named operator.
-;;;;       Output appears in Pigiron terminal.
-;;;;
-;;;;   (pig:q-channels)
-;;;;       Display info for filter and distributor operators in the
-;;;;       Pigiron terminal.   The display will included enabled
-;;;;       input and output MIDI channels.
-;;;;
-;;;;   (pig:monitor-exclude status flag)
-;;;;       Enable/Disable monitoring of specific MIDI status types.
-;;;;
-;;;;   (pig:monitor-on)
-;;;;       Enable Pigiron monitor.
-;;;;
-;;;;  (pig:monitor-off)
-;;;;      Disable Pigiron monitor.
-;;;;
-;;;;  (pig:in-channels  &rest channels)
-;;;;      Select Pigiron MIDI input channels.
-;;;;
-;;;;  (pig:out-channels &rest channels)
-;;;;      Select Pigiron MIDI output channels.
-;;;;
-;;;;  The following functions transmit single MIDI message via Pigiron's
-;;;;  output operator.  All channels in range [1,16] inclusive.
-;;;;  All data bytes (with exception for bend) 0 <= data < 0x80.
-;;;;      
-;;;;  (pig:note-off channel key &optional velocity)
-;;;;  (pig:note-on channel key &optional velocity)
-;;;;  (pig:poly-pressure channel key pressure)
-;;;;  (pig:controller channel controller-number value)
-;;;;  (pig:program channel program-number)
-;;;;  (pig:mono-pressure channel pressure)
-;;;;
-;;;;  Bend transmits pitch-bend event.  normal-bend is a float
-;;;;  -1.0 <= normal-bend <= +1.0.
-;;;;
-;;;;  (pig:bend channel normal-bend)
-;;;;
-;;;;  Transmit System Exclusive message, do not include either
-;;;; SYSEX or END-OF-SYSEX status bytes.
-;;;;
-;;;; (pig:sysex bytes)
-;;;;
-
-
-(defpackage :pig
-  (:use :cl)
-  (:import-from :cyco
-		:param
-		:->vector
-		:->list
-		:->string
-		:name
-		:constant
-		:global
-		:load-plugin-file
-		:property
-		:*project*
-		:join-path
-		:cyco-error
-		:sformat))
-
-(in-package :pig)
 
 (load "~/quicklisp/setup")
 (eval-when (:compile-toplevel :load-toplevel :execute)
@@ -119,130 +11,167 @@
   (ql:quickload :usocket)
   (ql:quickload :uiop))
 
-(defclass server-proxy nil
-  ((prefix
-    :type string
-    :initform "/pig"
-    :accessor osc-prefix
-    :initarg :prefix)
-   (host
-    :type vector
-    :initform #(127 0 0 1)
-    :accessor pig-host
-    :initarg :host)
-   (port
-    :type integer
-    :initform 8020
-    :accessor pig-port
-    :initarg :port)
-   (response-file
-    :type string
-    :initform "~/.config/pigiron/response"
-    :accessor response-filename
-    :initarg :response-file)))
+(global *pig-sleep-time* 1.0)
+(global *pig-prefix* "/pig")
+(global *pig-host* #(127 0 0 1))
+(global *pig-port* 8020)
+(global *pig-output-channels* '(1))
+(global *pig-midi-filename* "")
+(global *pig-player-op* "player")
+(global *pig-output-op* "out")
+(global *pig-distributor-op* "dist")
 
-;; Default server-proxy
-;;
-(global *server-proxy* nil)
+(labels ((format-address (command)
+			 (sformat "~A/~A" *pig-prefix* command))
+	 
+	 (bool->str (flag) (if flag "true" "false"))
+
+	 ;; ISSUE: Not portability 
+	 (is-absolute-filename-p (filename)
+				 (and (plusp (length filename))
+				      (or (eq (char filename 0) #\/)
+					  (eq (char filename 0) #\~))))
+	 
+	 ;; If name is relative to current project, do not add .mid extension
+	 ;;
+	 (resolve-midi-filename (&optional name)
+				(cond ((is-absolute-filename-p name) name)
+				      ((and *project* name)
+				       (join-path (property *project* :project-directory)
+						  (string-downcase (->string (name *project*)))
+						  (property *project* :output-directory)
+						  (sformat "~A.mid" name)
+						  :as-file))
+				      (*project*
+				       (let ((s (property *project* :current-section)))
+					 (if s (join-path  (property *project* :project-directory)
+							   (string-downcase (->string (name *project*)))
+							   (property *project* :output-directory)
+							   (string-downcase (sformat "~A.mid" (name s)))
+							   :as-file))))
+				      (t nil)))
+	 
+	 (midi->pig (&rest bytes)
+		    (let ((acc (sformat "midi, ~A, " *pig-output-op*)))
+		      (dolist (b bytes)
+			(setf acc (sformat "~A ~A," acc b)))
+		      (setf acc (subseq acc 0 (1- (length acc))))
+		      (pig-send "exec" acc)))
+	 
+	 (->status (channel stat)
+		   (let ((ci (logand (1- channel) #x0F)))
+		     (logior stat ci))) )
+	 
+  (defun pig-send (command &optional (data 0))
+    (let* ((address (format-address command))
+	   (socket (usocket:socket-connect *pig-host*
+					   *pig-port*
+					   :protocol :datagram
+					   :element-type '(unsigned-byte 8)))
+	   (message (apply #'osc:encode-message (cons address (list data)))))
+      (unwind-protect
+	  (progn
+	    (usocket:socket-send socket message (length message))
+	    (format t "PIG: ~A  data ~A~%" address data)
+	    (when socket (usocket:socket-close socket))))))
+
+  (defun pig-ping ()
+    (pig-send "ping"))
 
 
-(defun server-proxy (&key (prefix "/pig")(host #(127 0 0 1))(port 8020)
-			  (response-file "~/.config/pigiron/response"))
-  "(PIG:SERVER-PROXY &key prefix host port)
-Sets parameters for Pigiron server.
-:key  - OSC address prefix, default /pig
-:host - ip address, default #(127 0 0 1)
-:port - port number, default 8020"
-  (setf *server-proxy* (make-instance 'server-proxy
-				    :prefix prefix
-				    :host host
-				    :port port
-				    :response-file response-file))
-  *server-proxy*)
+  (defun pig-load-smf (name)
+    (let ((filename (resolve-midi-filename name)))
+      (if filename
+	  (progn
+	    (setf *pig-midi-filename* filename)
+	    (pig-send "exec" (sformat "op ~A, load, ~A" *pig-player-op* filename))
+	    (format t "Pigiron MIDI filename is: ~A~%" filename)
+	    (sleep 0.1)
+	    t)
+	(progn
+	  (cyco-error (sformat "Pigiron can not load MIDI file: ~A~%" name))
+	  nil))))
+  
 
-(server-proxy)
-
-
-(flet ((format-address (command)
-		       (sformat "~A/~A" (osc-prefix *server-proxy*) command)))
-
-      (defun osc-send (command &optional (data 0))
-	;; data can not be empty
-	(let* ((address (format-address command))
-	       (socket (usocket:socket-connect (pig-host *server-proxy*)
-					       (pig-port *server-proxy*)
-					       :protocol :datagram
-					       :element-type '(unsigned-byte 8)))
-	       (message (apply #'osc:encode-message (cons address (list data)))))
-	  (unwind-protect
-	      (progn 
-		(usocket:socket-send socket message (length message))
-		(format t "PIG: ~A  data ~A~%" address data)
-	    (when socket (usocket:socket-close socket)))))))
+  (defun stop ()
+    (pig-send "exec" (sformat "op ~A, stop" *pig-player-op*))
+    (sleep *pig-sleep-time*))
 
 
-(defun bool->str (flag)
-  (if flag
-      "true"
-    "false"))
+  (defun play (&optional name)
+    (stop)
+    (if name
+	(pig-load-smf name))
+    (pig-send "exec" (sformat "op ~A, play" *pig-player-op*)))
+      
 
-(load-plugin-file "player")
-(load-plugin-file "midi")
-(load-plugin-file "monitor")
-(load-plugin-file "filter")
-(load-plugin-file "distributor")
+  (defun output-channel (inst)
+    (setf *pig-output-channels* '())
+    (dolist (q (->list inst))
+      (let ((c (channel q :resolve)))
+	(push c *pig-output-channels*)))
+    (pig-send "exec" (sformat "deselect-all-channels ~A" *pig-distributor-op*))
+    (dolist (c *pig-output-channels*)
+      (sleep 0.1)
+      (pig-send "exec" (sformat "select-channel, ~A, ~A" *pig-distributor-op* c)))
+    *pig-output-channels*)
+
+  
+  (defun pig-notes (keylist &key (vel 64)(dur 1.0))
+    (let ((keys (keynumber (->list keylist)))
+	  (v (truncate (logand vel #x7F))))
+      (dolist (k keys)
+	(dolist (c (->list *pig-output-channels*))
+	  (let ((status (->status c #x90)))
+	    (midi->pig status k v))))
+      (sleep dur)
+      (dolist (k keys)
+	(dolist (c (->list *pig-output-channels*))
+	  (let ((status (->status c #x80)))
+	    (midi->pig status k 0))))))
+      
+
+  (defun pig-cc (controller value)
+    (let ((ctrl (get-controller-number controller))
+	   (val (truncate (logand value #x7f))))
+       (dolist (c (->list *pig-output-channels*))
+	 (midi->pig (->status c #xB0) ctrl val))))
+	  
+
+  (defun pig-program (pnumber)
+    (let ((n (truncate (logand pnumber #x7f))))
+      (dolist (c (->list *pig-output-channels*))
+	(midi->pig (->status c #xC0) n))))
+
+
+  (defun pig-bend (normal-bend)
+    (let* ((bytes (bend->midi-data normal-bend))
+	   (msb (aref bytes 0))
+	   (lsb (aref bytes 1)))
+      (dolist (c (->list *pig-output-channels*))
+	(midi->pig (->status c #xE0) msb lsb))))
+
+
+  (defun pig-sysex (bytes &optional raw)
+    (let ((lst (if raw
+		   (->list bytes)
+		 (append '(#xF0) (->list bytes) '(#xF7)))))
+    (apply #'midi->pig lst))) )
+
+
+;;; Conveniences functions
+;;;
+
+(defun s ()
+  (stop))
+
+(defun p (&optional name)
+  (play name))
+
+(defun lpp (&optional name)
+  (lpf)
+  (play name))
+
+
 (load-plugin-file "docs")
 
-(defun ping ()
-  "(PIG:PING) Sends ping message to Pigiron server."
-  (osc-send "ping"))
-
-(defun exit ()
-  "(PIG:EXIT) Sends exit message to Pigiron server."
-  (osc-send "exit"))
-
-(defun info (what)
-  (osc-send "info" what))
-
-(defun q-channels ()
-  (osc-send "info" *filter*)
-  (osc-send "info" *distributor*))
-
-(export '(*midi-op*
-          *midi-player*
-	  *monitor*
-	  *filter*
-	  *distributor*
-	  ?pig
-          bend
-          controller
-          exit
-          load-smf
-          mono-pressure
-          note-off
-	  note-on
-     	  server-proxy
-          ping
-          play
-	  player-ping
-          poly-pressure
-          program
-          stop
-	  monitor-exclude
-	  monitor-on
-	  monitor-off
-	  monitor-log
-	  monitor-close-log
-          sysex
-	  info
-	  in-channels
-	  out-channels
-	  q-channels)
-	:pig)
-
-(import '(pig:?pig
-	  pig:play
-	  pig:stop)
-	:cyco)
-
-(in-package :cyco)

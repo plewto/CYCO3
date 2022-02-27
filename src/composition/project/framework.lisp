f69e030a91d41cc1c28b92efd43131640f382a08
Steven Jones <plewto@gmail.com>
2022-02-27 16:24:22 -0600

    Changes load-project to take project number as an argument.

--- src/composition/project/framework.lisp
+++ src/composition/project/framework.lisp
@@ -5,31 +5,30 @@
 
 (in-package :cyco)
 
-(defun ?projects ()
-  "Displays contents of *PROJECTS-ROOT*"
-  (format t "Contents of project directory ~A~%" *projects-root*)
-  (format t "*PROJECT-MAIN-FILENAME-FORMAT* is ~A~%" *project-main-filename-format*)
-  (format t "*DEFAULT-PROJECT-OUTPUT-DIRECTORY* is ~A~%" *default-project-output-directory*)
-  (dolist (fqn (directory (sformat "~a/*" *projects-root*)))
-    (let* ((nstr (namestring fqn))
-	   (pname (second (split-path (subseq nstr 0 (1- (length nstr)))))))
-      (format t "  ~A~%" pname))))
+(defun ?projects (&optional (print t))
+  (let ((count 0)
+	(acc '()))
+    (dolist (fqn (directory (sformat "~A/*" *projects-root*)))
+      (let* ((nstr (namestring fqn))
+	     (pname (second (split-path (subseq nstr 0 (1- (length nstr)))))))
+	(push (cons count pname) acc)
+	(setf count (1+ count))))
+    (setf acc (reverse acc))
+    (if print
+	(prog1
+	     (format t "Contents of project directory ~A~%" *projects-root*)
+	  (format t "*PROJECT-MAIN-FILENAME-FORMAT* is ~A~%" *project-main-filename-format*)
+	  (format t "*DEFAULT-PROJECT-OUTPUT-DIRECTORY* is ~A~%" *default-project-output-directory*)
+	  (dolist (item acc)
+	    (format t "[~3D] ~A~%" (car item)(cdr item))))
+      acc)))
+
 	   
 
 (flet ((format-name (sym)
 		    (string-downcase (->string sym))))
 
   (defun create-project-framework (files)
-    "Creates empty project directory.
-The files argument specifies the project name and any additional empty files 
-to be created.   
-
-(create-project-framework 'foo)
-    Create project directory foo with main file and MIDI directory.
-
-(create-project-framework '(foo bar baz))
-    As above but also creates empty files bar.lisp and baz.lisp"
-
     (let* ((flist (->list files))
 	   (project-name (car flist))
 	   (extras (cdr flist))
@@ -59,3 +58,30 @@ to be created.
 		  	(format stream (sformat "(project ~A)~%" project-name))))
 		  (close stream)))
   	    (format t "Project file already exists: ~A~%" fqn)))))))
+
+
+;; Docstrings
+
+
+(setf (documentation '?projects 'function)
+      "Displays contents of *PROJECTS-ROOT*
+Each item is preceded by an index number.
+Please note, --ALL-- sub-directories under *PROJECTS-ROOTS* are included 
+regardless if they are actual project folders or not.
+
+If optional print argument is nil, returns list of directories instead of 
+printing them. 
+
+The project number may be used as an argument to LOAD-PROJECT and LP.")
+
+
+(setf (documentation 'create-project-framework 'function)
+         "Creates empty project directory.
+The files argument specifies the project name and any additional empty files 
+to be created.   
+
+(create-project-framework 'foo)
+    Create project directory foo with main file and MIDI directory.
+
+(create-project-framework '(foo bar baz))
+    As above but also creates empty files bar.lisp and baz.lisp")
--- src/composition/project/loader.lisp
+++ src/composition/project/loader.lisp
@@ -9,50 +9,25 @@
 (global *current-lpf-filename* nil)  ;; ISSUE: Dont like this as a global.
 
 (let ((current-project-main-file nil)
-      (frmt "Loading project file ~A~%")
+      (frmt "Loading project file ~A~%") )
       
-      (load-project-docstring
-        "Loads the main project file.
+  (labels ((default-project-name (name)
+	     (string-downcase
+	      (->string (or name
+			    (and (project-p *project*)(name *project*))
+			    (read-persistent-project-name)))))
 
-name should be a symbol matching the project's directory.
-
-The optional project-directory and main-file arguments are used to 
-specify a non-standard location.
-
-The CWD is set to the projects location. 
-
-See LP as a more convenient method of loading a project.")
-      
-      (lp-docstring
-         "LP is a short-cut for load-project with useful defaults.
-The first time it is used the optional name argument should be specified as in 
-load-project.  Thereafter subsequent calls to LP will default to reloading the 
-current project.  
-
-Whenever a new project is loaded its name is saved to a file in the configuration
-directory.  The next time CYCO is ran the previous project may be reloaded simply 
-by calling LP without an argument.")
-      
-      (load-project-file-docstring
-         "load-project-file loads a lisp file relative to the project's directory.
-The name argument is a symbol which is converted to a lowercase string.
-See LPF for a convenient shortcut.")
-      
-     (lpf-docstring
-         "LPF is a short-cut for load-project-file.  When used without an argument
-it reloads the most recent project-file.  Do not quote the argument."))
-
-      
-  (flet ((default-project-name (name)
-	   (string-downcase
-	    (->string (or name
-			  (and (project-p *project*)(name *project*))
-			  (read-persistent-project-name))))))
+	   (project-name-by-number (n)
+				   (let* ((plst (?projects nil))
+					  (name (cdr (nth n plst))))
+				     (or name
+					 (cyco-warning (sformat "Illegal project number: ~A" n))))) )
   
     (defun load-project (name &key
 			      (project-directory *projects-root*)
 			      main-file)
-      load-project-docstring
+      (if (integerp name)
+	  (setf name (project-name-by-number name)))
       (let ((project-name (default-project-name name)))
 	(if project-name
 	    (let ((fqn (join-path project-directory project-name
@@ -72,14 +47,10 @@ it reloads the most recent project-file.  Do not quote the argument."))
     
     (defmacro lp (&optional name)
       `(if ',name
-	   (load-project ',name)
-	 (load-project nil)))
-
-    (setf (documentation 'lp 'function)
-	  lp-docstring)
+    	   (load-project ',name)
+    	 (load-project nil)))
     
     (defun load-project-file (name)
-      load-project-file-docstring
       (let ((project-file-name
 	     (cond ((symbolp name)
 		    (string-downcase (->string name)))
@@ -102,7 +73,39 @@ it reloads the most recent project-file.  Do not quote the argument."))
 	     (progn 
 	       (format t "Reloading ~A~%" *current-lpf-filename*)
 	       (load-project-file *current-lpf-filename*))
-	   (cyco-composition-error 'lpf ',name "Section does not exixts."))))
+	   (cyco-composition-error 'lpf ',name "Section does not exixts.")))) ))
+
+;; Docstrings
+
+(setf (documentation 'load-project 'function)
+      "Loads the main project file.
+
+name should be a symbol matching the project's directory.
+Alternativly name may be the project number as displayed by ?PROJECTS function.
+
+The optional project-directory and main-file arguments are used to 
+specify a non-standard location.
+
+The CWD is set to the projects location. 
+
+See LP as a more convenient method of loading a project.")
 
 
-    (setf (documentation 'lpf 'function) lpf-docstring)))
+(setf (documentation 'lp 'function)
+      "LP is a short-cut for load-project with useful defaults.
+The first time it is used the optional name argument should be specified as in 
+load-project.  Thereafter subsequent calls to LP will default to reloading the 
+current project.  
+
+Whenever a new project is loaded its name is saved to a file in the configuration
+directory.  The next time CYCO is ran the previous project may be reloaded simply 
+by calling LP without an argument.")
+
+(setf (documentation 'load-project-file 'function)
+      "load-project-file loads a lisp file relative to the project's directory.
+The name argument is a symbol which is converted to a lowercase string.
+See LPF for a convenient shortcut.")
+
+(setf (documentation 'lpf 'function)
+      "LPF is a short-cut for load-project-file.  When used without an argument
+it reloads the most recent project-file.  Do not quote the argument.")
